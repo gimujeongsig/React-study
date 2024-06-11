d33834dada36e9787403f4872dd14295105f4d6b
gimujoengsig <dbehcuwjc@gmail.com>
2024-06-11 21:07:49 +0900

    6_11

---
+++ 6_11/fitstapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+// import Say from './Say'
+import EventPratics from './EventPratice';
+
+const App = () => {
+  return <EventPratics />;
+};
+
+
+export default App;
+
+  
