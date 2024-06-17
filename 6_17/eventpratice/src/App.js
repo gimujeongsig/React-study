e18470204bdc73c543adcd5dfc00e41d29123806
gimujeongsig <dbehcuwjc@gmail.com>
2024-06-17 21:13:52 +0900

    정식정식

---
+++ 6_17/eventpratice/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import EventPratice from './eventpratice';
+import Event2 from './event2';
+
+function App() {
+  return (
+    <div className='App'>
+      <Event2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 6_17/eventpratice/src/eventpratice.js
@@ -0,0 +1,60 @@
+import { Component } from "react";
+// 클래스형 컴포넌트
+
+class EventPratice extends Component {
+    state = {
+        "message" : '',
+        "username" : ''
+    }
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + ': ' + this.state.message);
+        this.setState({
+            message : '',
+            username : ''
+        });
+    }
+    
+    handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            this.handleClick();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="사용자 명"
+                    value={username}
+                    onChange={onChange}
+                    ></input>
+
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력하세요"
+                    value={message}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                    ></input>
+                <button onClick={this.handleClick}>확인</button>
+            </div>
+        )
+    }
+}
+export default EventPratice;
