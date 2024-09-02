ef76c7dec942ec69f91d0a94e2f673c9eef7c5e8
gimujeongsig <dbehcuwjc@gmail.com>
2024-09-02 20:13:30 +0900

    9_2

--- shop/src/App.js
+++ shop/src/App.js
@@ -35,11 +35,15 @@ function App() {
           </div>
           </>
         }></Route>
-        <Route path="/detail" element={<Detail/>}></Route>
+        <Route path="/detail" element={<Detail shoes={shoes}/>}></Route>
         <Route path="*" element={<div>없는 페이지입니다 404 Error</div>}></Route>
         <Route path="/about" element={ <About/>}>
           <Route path="member" element={<div>멤버입니다</div>}></Route>
           <Route path="location" element={<About/>}></Route>
+        </Route>
+        <Route path='/event' element={<EventPage></EventPage>}>
+          <Route path='one' element={<p>첫 주문시 신발 1+1 이벤트</p>}></Route>
+          <Route path='two' element={<p>생일 기념 쿠폰 발급하기</p>}></Route>
         </Route>
       </Routes>
 
@@ -54,6 +58,16 @@ function About () {
     </div>
   )
 }
+
+function EventPage() {
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
 function Card(props) {
   return (
     <div className='col-md-4'>
